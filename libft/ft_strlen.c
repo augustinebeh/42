97bb011a939503263a307755f7c96390fb2f7b0f
Augustine Beh <enitsugua.b@gmail.com>
2024-05-15 03:03:13 +0800

    Update ft_strlen.c

--- libft/ft_strlen.c
+++ libft/ft_strlen.c
@@ -10,3 +10,29 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+#include <stdio.h>
+
+int ft_strlen(char *str)
+{
+    int i = 0;
+    while (str[i] != '\0')
+    {
+        i++;
+    }
+    return(i);
+}
+/*find out why the length of string is way more than it should be if input is: "1//"*/
+/* int main(int argc, char **argv)
+{
+    if (argc != 2)
+    {
+        printf("Error\n");
+    }
+    else
+    {
+        printf("------------------------------------------------\n");
+        printf("Input value = %s\nLength of the Input: %d\n", argv[1], ft_strlen(argv[1]));
+        printf("------------------------------------------------\n");
+    }
+} */
