0724dee4fb805176d913ca3d194d633408c37b1d
Augustine Beh <enitsugua.b@gmail.com>
2024-05-15 02:32:36 +0800

    Update ft_atoi.c

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -13,26 +13,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int ft_atoi(char *s)
+int ft_atoi(char *c)
 {
     int i;
     int sign;
     int result;
 
     i = 0;
-    sign = 1;
+    sign = 1;   
     result = 0;
-    while (s[i] == ' ' || s[i] >= 9 && s[i] <= 13)
+    while (c[i] == ' ' || c[i] >= 9 && c[i] <= 13)
         i++;
-    if (s[i] == '-' || s[i] == '+')
+    if (c[i] == '-' || c[i] == '+')
     {
-        if (s[i] == '-')
+        if (c[i] == '-')
             sign = sign * -1;
         i++;
     }
-    while (s[i] >= '0' && s[i] <= '9')
+    while (c[i] >= '0' && c[i] <= '9')
     {
-        result = result * 10 + (s[i] - '0');   
+        result = result * 10 + (c[i] - '0');   
         i++;
     }
     result = result * sign;
