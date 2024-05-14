b9aa31f0566ca080dcf48cc4700086f93d97b947
Augustine Beh <enitsugua.b@gmail.com>
2024-05-15 03:59:29 +0800

    update ft_toupper.c

--- libft/ft_toupper.c
+++ libft/ft_toupper.c
@@ -10,3 +10,40 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+#include <stdio.h>
+
+char* ft_toupper(char* c)
+{
+    int i;
+    
+    i = 0;
+    while (c[i] != '\0')
+    {
+        if (c[i] >= 'a' && c[i] <= 'z')
+        {
+            c[i] = c[i] - 32;
+        }
+        i++;
+    }
+    return c;
+}
+
+/* int main(int argc, char **argv)
+{
+    if (argc != 2)
+    {
+        printf("Error\n");
+    }
+    else
+    {
+        char* result;
+
+        result = ft_toupper(argv[1]);
+        printf("Input is: %s\n", argv[1]);
+        printf("------------------------------------------------\n");
+        printf("Uppercase equivalent of %s is %s\n", (argv[1]), result);
+        printf("------------------------------------------------\n");
+        return (0);
+    }
+} */
