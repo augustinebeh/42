138da9bf07dfea67819bf8e15523a5e039cbdbb0
Augustine Beh <enitsugua.b@gmail.com>
2024-05-15 06:49:48 +0800

    Update ft_strrchr.c

--- libft/ft_strrchr.c
+++ libft/ft_strrchr.c
@@ -10,3 +10,60 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+
+char *ft_strrchr(const char *str, int c)
+{
+    int i;
+    int j;
+    int k;
+    int len;
+
+    j = 0;
+    i = strlen(str);
+    k = 0;
+    len = strlen(str);
+    char *str2 = (char *)malloc(sizeof(char) * (len + 2));
+    while (i >= 0)
+    {
+        if (str[i] == c)
+        {
+            j = i;
+            break;
+        }
+        i--;
+    }
+    while (j <= len)
+    {
+        str2[k] = str[j];
+        j++;
+        k++;
+    }
+    str2[k] = '\0';
+    return (str2);
+}
+
+/* int main(int argc, char **argv)
+{
+    if (argc != 3 || argv[2][1] != '\0')
+        printf("----------------------ERROR---------------------\n");
+    else
+    {   
+        char *str;
+        char *str2;
+        
+        str = ft_strrchr(argv[1], argv[2][0]);
+        str2 = strrchr(argv[1], argv[2][0]);
+        printf("------------------------------------------------\n");
+        printf("Input String = %s\n", argv[1]);
+        printf("Input tofind char: %s\n", argv[2]);
+        printf("---------------------\n");
+        printf("After ft_strrchr: %s\n", str);
+        printf("After real strrchr: %s\n", str2);
+        printf("------------------------------------------------\n");
+    }
+} */
