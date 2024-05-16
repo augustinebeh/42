a0c02ec2ed8dcecfa42e6b5b0a550dd3cd9b8a4b
Augustine Beh <enitsugua.b@gmail.com>
2024-05-16 18:52:00 +0800

    Update ft_strdup.c -incomplete-

--- libft/ft_strdup.c
+++ libft/ft_strdup.c
@@ -10,3 +10,40 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+char *ft_strdup(const char *s)
+{
+    int len;
+    char* str;
+
+    len = strlen(s);
+    str = (char *) malloc(sizeof(char) * len + 1);
+    if (str == NULL)
+        return NULL;
+    strcpy(str, s);
+    return str;
+}
+
+int main(int argc, char **argv)
+{
+    if (argc == 2)
+    {   
+        printf("Input  : %s\n", argv[1]);
+        char* i;
+        i = ft_strdup(argv[1]);
+        if (i == NULL)
+        {
+            printf("Memory allocation failed\n");
+            return 1;
+        }
+        printf("Input  : %s\n", i);
+        free(i);
+        return 0;
+    }
+    else
+        printf("----------------------ERROR---------------------\n");
+}
