ea71d05902772b72bd01e25e55a9a2cfa5df8fa5
Augustine Beh <enitsugua.b@gmail.com>
2024-05-16 17:58:48 +0800

    Update ft_memset.c

--- libft/ft_memset.c
+++ libft/ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: abeh <abeh@student.42singapore.sg>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/14 22:35:04 by abeh              #+#    #+#             */
-/*   Updated: 2024/05/14 22:35:04 by abeh             ###   ########.fr       */
+/*   Updated: 2024/05/16 17:58:26 by abeh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,19 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-// void		*ft_memset(void *ptr, int c, size_t len)
+void		*ft_memset(void *ptr, int c, size_t len)
+{
+    size_t		i;                  // to match with size_t len
+    i = 0;                          
+    while (i < len)
+    {
+        ((char *)ptr)[i] = c;       // cast char* to void
+        i++;
+    }
+    return(ptr);
+}
 
-int main(int argc, char **argv)
+/* int main(int argc, char **argv)
 {
     if (argc != 4)
     {
@@ -29,20 +39,22 @@ int main(int argc, char **argv)
         char *str = malloc(len + 1);       //allocate a memory for str based on the length of str + 1 is ofr the null byte
         char *str2 = malloc(len + 1);      //allocate a memory for str based on the length of str + 1 is ofr the null byte
         strcpy(str, argv[1]);              //copy arrgv[1] into str
-        strcpy(str2, "excuseme");
+        strcpy(str2, argv[1]);              //copy arrgv[1] into str2
         if (str == NULL)                   //ensuring that memory is being allocated for str
         {
             fprintf(stderr, "Memory allocation failed\n");
             return(1);
         }
-        printf("Input str         >> %s\n", argv[1]);
+        printf("------------------------------------------------\n");
+        printf("Input string      >> %s\n", argv[1]);
 
         int t = atoi(argv[3]);             //convert argv[3] into integer to use in memset function
         char c = argv[2][0];               //unnecessary -- only to simplify the variable for memset function
         memset(str, c, t);                 //applying built-in memset to the str
-        my_memset(str2, c, t);             //applying my created memset to the str
+        ft_memset(str2, c, t);             //applying my created memset to the str
         printf("After real memset >> %s\n", str); //prints the output of the built-in memset function
-        printf("After my_memset >> %s\n", str2); //prints the output of my created memset function
+        printf("After ft_memset   >> %s\n", str2); //prints the output of my created memset function
+        printf("------------------------------------------------\n");
         return 0;
     }
-}
+} */
