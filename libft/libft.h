224c2d7a43c8bd8c0f9b1a19cbe81aafdc6616d2
Augustine Beh <enitsugua.b@gmail.com>
2024-05-16 19:04:09 +0800

    Update libft.h

--- libft/libft.h
+++ libft/libft.h
@@ -3,12 +3,19 @@
 
 # include <unistd.h>
 
-int ft_atoi(char *c);
-int ft_isalnum(int c);
-int ft_isalpha(int c);
-int ft_isascii(int c);
-int ft_isdigit(int c);
-int ft_isprint(int c);
+int ft_atoi(const char *c);
+
+int					ft_isupper(int c);
+int					ft_islower(int c);
+int					ft_isalpha(int c);
+int					ft_isdigit(int c);
+int					ft_isalnum(int c);
+int					ft_isascii(int c);
+int					ft_isprint(int c);
+int					ft_isspace(int c);
+int					ft_toupper(int c);
+int					ft_tolower(int c);
+
 char *ft_itoa(int n);
 void ft_putchar_fd(char c, int fd);
 void ft_putendl_fd(char *s, int fd);
@@ -26,8 +33,8 @@ char **ft_split(char const *s, char c);
 char *ft_strtrim(char const *s1, char const *set);
 char *ft_substr(char const *s, unsigned int start, size_t len);
 char *ft_strchr(const char *s, int c);
-size_t ft_strlen(char *str);
-int ft_tolower(int c);
-int ft_toupper(int c);
+size_t				ft_strlen(const char *s);
+size_t				ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t				ft_strlcat(char *dst, const char *src, size_t size);
 
 # endif
