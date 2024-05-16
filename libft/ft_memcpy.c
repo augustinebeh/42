fd807cb764b40c0d12fb8f7934d5180b891418b1
Augustine Beh <enitsugua.b@gmail.com>
2024-05-16 20:59:28 +0800

    Update ft_memcpy.c

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -10,3 +10,74 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void *ft_memcpy(void *dest, const void *src, size_t n)
+{
+    size_t i;
+
+    i = 0;
+    while (src && i < n)
+    {
+        ((char *)dest)[i] = ((char *)src)[i];
+        i++;
+    }
+    return (dest);
+}
+
+//alternative method to obtain same result
+/* void *ft_memcpy(void *dest, const void *src, size_t n)
+{
+    // Typecast src and dest addresses to (char *)
+    char *csrc = (char *)src;
+    char *cdest = (char *)dest;
+
+    // Copy contents of src[] to dest[]
+    for (int i=0; i<n; i++)
+        cdest[i] = csrc[i];
+
+    //Return the destination address
+    return dest;
+} */
+
+//main file for manual testcases. argc based on number of input variables
+/* int main(int argc, char **argv)
+{
+    if (argc != 4)
+        printf("----------------------ERROR---------------------\n");
+    else
+    {
+        size_t len = strlen(argv[1]);
+        size_t len2 = strlen(argv[3]);
+        char *d = malloc(sizeof(char) * (len + 1));
+        if (!d)
+        {
+            fprintf(stderr, "Memory allocation failed for d\n");
+            return 1;
+        }
+
+        char *s = malloc(sizeof(char) * (len + 1));
+        if (!s)
+        {
+            fprintf(stderr, "Memory allocation failed for s\n");
+            return 1;
+        }
+        strcpy(d, argv[1]);
+        strcpy(s, argv[2]);
+        ft_memcpy(d, s, atoi(argv[3]));
+        printf("----------------------------------------\n");
+        printf("Input               : %s\n", argv[1]);
+        printf("----------------------------------------\n");
+        printf("Output(ft_memcpy)   : %s\n", d);
+        
+        memcpy(d, s, atoi(argv[3]));
+        printf("Output(memcpy)      : %s\n", d);
+        printf("----------------------------------------\n");
+        free(d);
+        free(s);    
+        return 0;
+    }
+} */
