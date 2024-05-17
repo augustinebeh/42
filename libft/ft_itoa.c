7392e3abf9f3fabb57936473af8f657128924e62
Augustine Beh <enitsugua.b@gmail.com>
2024-05-17 12:53:44 +0800

    Update ft_itoa.c

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: abeh <abeh@student.42singapore.sg>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 04:22:22 by abeh              #+#    #+#             */
-/*   Updated: 2024/05/17 05:14:08 by abeh             ###   ########.fr       */
+/*   Updated: 2024/05/17 12:26:34 by abeh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,6 @@ int ft_intlen(int p)
 
     return (i);
 }
-
 // main code to test ft_intlen
 /* int main(void)
 {
@@ -49,12 +48,14 @@ char *ft_itoa(int n)
     if (result < 0)            // Convert the value of result from a "-ve" to a "+ve"
     {
         result = result * -1;
-        j = 1;                 // if input is "-ve", j=1 | if input is "+ve", j=0
+        j = 1;                 // If input is "-ve", j=1 | if input is "+ve", j=0
     }
     len = ft_intlen(result);
     len2 = len;           
     result1 = malloc(sizeof(char) * len + 1);
-    while (len + 1 + j > 0)   // add j to this equation to increment result1 further back by 1
+    if (result1 == NULL)       // Check if allocation failed
+        return NULL;           // Return NULL
+    while (len + 1 + j > 0)    // Add j to this equation to increment result1 further back by 1
     {
         result1++;
         len--;
@@ -71,7 +72,7 @@ char *ft_itoa(int n)
             len2--;
         }
     }
-    if (j == 1)               //if input is negative, add "-"
+    if (j == 1)                //If input is negative, add "-"
         *result1 = '-';
     return (result1);
 }
@@ -80,15 +81,15 @@ int main(int argc, char **argv)
 {
     if (argc != 2)
     {
-        printf("----------------------ERROR---------------------\n");
+        printf("-----ERROR-----\n");
     }
     else
     {
         int c = atoi(argv[1]);
 
         char *hehe = ft_itoa(c);
-        printf("The input is: %s\n", argv[1]);
-        printf("The output is: %s\n", hehe);
+        printf("Input is: %s\n", argv[1]);
+        printf("Output is: %s\n", hehe);
     }
     return (0);
 }
