81fc7f6e6e259aee09f382a1f41e35e2bbe27310
Augustine Beh <abeh@c2r4s13.42singapore.sg>
2024-05-17 15:30:30 +0800

    Update ft_itoa.c

---
+++ libft/ft_itoa.c
@@ -0,0 +1,104 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abeh <abeh@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/17 05:01:04 by abeh              #+#    #+#             */
+/*   Updated: 2024/05/17 15:24:15 by abeh             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int	ft_intlen(int p)
+{
+	int	i;
+
+	i = 0;
+	while (p != 0)
+	{
+		p = p / 10;
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_itoa_positive(int n)
+{
+	char	*res;
+	int		len;
+	int		len2;
+
+	len = ft_intlen(n);
+	len2 = len;
+	res = malloc(sizeof(char) * len + 1);
+	while (len-- + 1 > 0)
+		res++;
+	*res = '\0';
+	res--;
+	while (n != 0)
+	{
+		*res = (n % 10) + '0';
+		n = n / 10;
+		if (len2 - 1 > 0)
+		{
+			res--;
+			len2--;
+		}
+	}
+	return (res);
+}
+
+char	*ft_itoa_negative(int n)
+{
+	char	*res;
+	int		len;
+
+	n = n * -1;
+	len = ft_intlen(n);
+	res = malloc(sizeof(char) * len + 1);
+	while (len-- + 2 > 0)
+		res++;
+	*res = '\0';
+	res--;
+	len = len + 3 + ft_intlen(n);
+	while (n != 0)
+	{
+		*res = (n % 10) + '0';
+		n = n / 10;
+		if (len-- > 0)
+			res--;
+	}
+	*res = '-';
+	return (res);
+}
+
+char	*ft_itoa(int n)
+{
+	if (n < 0)
+		return (ft_itoa_negative(n));
+	else
+		return (ft_itoa_positive(n));
+}
+
+int	main(int argc, char **argv)
+{
+	char*hehe;
+
+	hehe = ft_itoa(argv[1]);
+	if (argc != 2)
+	{
+		printf("----------------------ERROR---------------------\n");
+	}
+	else
+	{
+		printf("The input is: %s\n", argv[1]);
+		printf("The output is: %s\n", hehe);
+	}
+	return (0);
+}
