4f803bc03d68541f13d42833242190258607751c
Augustine Beh <enitsugua.b@gmail.com>
2024-05-19 02:23:54 +0800

    Update ft_bzero.c

--- libft/ft_bzero.c
+++ libft/ft_bzero.c
@@ -10,3 +10,41 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+ #include <strings.h>
+ #include <stdio.h>
+ #include <stdlib.h>
+
+void ft_bzero(void *s, size_t n)
+{
+    size_t i;
+    unsigned char *str;
+
+    str = (unsigned char *)s;
+    while (n--)
+        *str++ = '\0';
+}
+
+// Change above ft_bzero function\s "\0" into a visible char to test
+int main(int argc, char **argv)
+{
+    if (argc != 3)
+    {
+        printf("--------ERROR-------\n");
+    }
+    else
+    {
+        char *str1 = argv[1];
+        size_t number = atol(argv[2]);
+
+        printf("---------------\n");
+        printf("Input str : %s\n", str1);
+        printf("Input size: %s\n", argv[2]);
+
+        ft_bzero(str1, number);
+
+        printf("\nAfter ft_bzero:\n");
+        printf("Output: %s\n", str1);
+        printf("---------------\n");
+    }
+    return 0;
+}
