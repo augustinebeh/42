b3fc217c9324e0a720f214b7964ecbdcb892ae3b
Augustine Beh <enitsugua.b@gmail.com>
2024-05-19 02:03:02 +0800

    Update ft_strlcat.c

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -36,7 +36,8 @@ size_t ft_strlcat(char *dst, const char *src, size_t size)
         dst[dstlen + i] = src[i];
         i++;
     }
-    dst[dstlen + i] = '\0';
+    if (dstlen < size)
+        dst[dstlen + i] = '\0';
     return (srclen + dstlen);
 }
 //----------------------------------------
