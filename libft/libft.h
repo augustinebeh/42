dcfbd1e7e2daacea8cf8fafb9810f78885975f83
Augustine Beh <enitsugua.b@gmail.com>
2024-05-18 19:01:20 +0800

    Update libft.h

--- libft/libft.h
+++ libft/libft.h
@@ -41,7 +41,7 @@ char *ft_strtrim(char const *s1, char const *set);
 char *ft_substr(char const *s, unsigned int start, size_t len);
 char *ft_strchr(const char *s, int c);
 size_t				ft_strlen(const char *s);
-size_t				ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t				ft_strlcpy(char *dst, const char *src, size_t size);
 size_t				ft_strlcat(char *dst, const char *src, size_t size);
 
 # endif
