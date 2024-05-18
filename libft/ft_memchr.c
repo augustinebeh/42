44e0e2d2ae9fbebb1c4273aa02f3d454a2da0610
Augustine Beh <enitsugua.b@gmail.com>
2024-05-18 16:20:07 +0800

    Update ft_memchr.c

--- libft/ft_memchr.c
+++ libft/ft_memchr.c
@@ -10,3 +10,46 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void *ft_memchr(const void *s, int c, size_t n)
+{
+    const unsigned char *p = s;
+    if (n > 0)
+    {
+        while (n > 0 && *p != (unsigned char)c)
+        {
+            p++;
+            n--;
+        }
+        return (void *)p;
+    }
+    else
+        return (0);
+}
+    
+int main(int argc, char **argv)
+{
+    if (argc != 4)
+        printf("-----------------ErRoR----------------\n");
+    else
+    {
+        char* y;
+        char* z;
+
+        printf("----------------------------------------\n");
+        printf("Input            : %s\n", argv[1]);
+        printf("----------------------------------------\n");
+
+        y = (char*)memchr(argv[1], (argv[2][0]), atoi(argv[3]));
+        printf("Output(memchr)   : %s\n", y);
+
+        z = (char*)ft_memchr(argv[1], (argv[2][0]), atoi(argv[3]));
+        printf("Output(ft_memchr): %s\n", y);
+
+        return(0);
+    }
+}
