919d108166ae4d6d3481055768a2ea8cf82b58cd
Augustine Beh <enitsugua.b@gmail.com>
2024-05-18 18:58:04 +0800

    Update ft_strlcpy.c

--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -34,7 +34,7 @@ int ft_strlcpy(char *dst, const char *src, size_t size)
 // Main code to test ft_strlcpy function |
 //----------------------------------------
 
-int main(int argc, char *argv[])
+/* int main(int argc, char *argv[])
 {
     if (argc != 4)
         printf("ERROR\n");
@@ -68,4 +68,4 @@ int main(int argc, char *argv[])
         }
         return (0);
     }
-}
+} */
