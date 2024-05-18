48056074f45f8466317581ee45f25c8dc3844962
Augustine Beh <enitsugua.b@gmail.com>
2024-05-19 01:09:07 +0800

    Update ft_strtrim.c UNCOMPLETE

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -6,8 +6,26 @@
 /*   By: abeh <abeh@student.42singapore.sg>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 04:23:56 by abeh              #+#    #+#             */
-/*   Updated: 2024/05/16 04:23:57 by abeh             ###   ########.fr       */
+/*   Updated: 2024/05/18 23:28:22 by abeh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char *ft_strtrim(char const *s1, char const *set);
+#include <stdio.h>
+#include <stdlib.h>
+
+char *ft_strtrim(char const *s1, char const *set)
+{
+    int i;
+    int j;
+    int k;
+    int len;
+    char *str;
+    len = ft_strlen(s1);
+    i = 0;
+    if (!s1 || !set)
+    {
+        return (0);
+    }
+    
+}
+
