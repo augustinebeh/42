c327c98d76217d0cbfd50e0fe3ee24a4443b62c5
Augustine Beh <enitsugua.b@gmail.com>
2024-05-19 15:47:16 +0800

    Update libft.h

--- libft/libft.h
+++ libft/libft.h
@@ -21,7 +21,7 @@ void				*ft_memcpy(void *dest, const void *src, size_t n);
 void				*ft_memccpy(void *dest, const void *src, int c, size_t n);
 void				*ft_memmove(void *dest, const void *src, size_t len);
 void				*ft_memchr(const void *s, int c, size_t n);
-void				*ft_calloc(size_t count, size_t size);
+void				*ft_calloc(size_t nmemb, size_t size);
 
 char *ft_itoa(int n);
 void ft_putchar_fd(char c, int fd);
