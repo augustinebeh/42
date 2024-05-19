0080e7597be48f7cc0f0ac7da9db01415cd2a717
Augustine Beh <enitsugua.b@gmail.com>
2024-05-20 03:53:03 +0800

    Update ft_toupper.c

--- libft/ft_toupper.c
+++ libft/ft_toupper.c
@@ -6,28 +6,28 @@
 /*   By: abeh <abeh@student.42singapore.sg>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/14 22:35:20 by abeh              #+#    #+#             */
-/*   Updated: 2024/05/14 22:35:20 by abeh             ###   ########.fr       */
+/*   Updated: 2024/05/20 03:48:57 by abeh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
-int ft_toupper(int c)
+int	ft_toupper(int c)
 {
-    if (c >= 'a' && c <= 'z')
-        return (c - 32);
+	if (c >= 'a' && c <= 'z')
+		return (c - 32);
 }
 
 // int main() {
 //     char *c = "DhI2wBe3d%";
 //     int i = 0;
 //     printf("The input is:  %s\nThe output is: ", c);
-    
+
 //     while (c[i] != '\0')
 //     {
 //         printf("%c", ft_toupper(c[i]));
 //         i++;
 //     }
 //     printf("\n");
-//     return 0;
+//     return (0);
 // }
