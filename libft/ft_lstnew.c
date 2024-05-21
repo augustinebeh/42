d545bc9655fa86c23abe512150af3ec9a18a89c9
Augustine Beh <enitsugua.b@gmail.com>
2024-05-21 16:33:40 +0800

    Update ft_lstnew.c

--- libft/ft_lstnew.c
+++ libft/ft_lstnew.c
@@ -6,22 +6,22 @@
 /*   By: abeh <abeh@student.42singapore.sg>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/21 03:08:24 by abeh              #+#    #+#             */
-/*   Updated: 2024/05/21 16:29:43 by abeh             ###   ########.fr       */
+/*   Updated: 2024/05/21 16:30:46 by abeh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list *ft_lstnew(void *content)
-{  
-    t_list *newnode;
-    
-    newnode = (t_list *)malloc(sizeof(t_list));
-    if (newnode == NULL)
-        return NULL;
-    newnode->content = content;
-    newnode->next = NULL;
-    return newnode;
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*newnode;
+
+	newnode = (t_list *)malloc(sizeof(t_list));
+	if (newnode == NULL)
+		return (NULL);
+	newnode->content = content;
+	newnode->next = NULL;
+	return (newnode);
 }
 
 /* int main()
@@ -31,12 +31,12 @@ t_list *ft_lstnew(void *content)
     if (node == NULL) // checks if the node is created successfully
     {
         printf("Failed to create a new node\n");
-        return 1;
+        return (1);
     }
     printf("Content of the node: %s\n", (char *)node->content);
     printf("Next of the node: %s\n", (char *)node->next);
 
     free(node);
 
-    return 0;
+    return (0);
 } */
