8fd595a6b64bf70ddc18cbc4c97b9a5faa73fef8
Augustine Beh <enitsugua.b@gmail.com>
2024-05-21 21:30:02 +0800

    Update ft_lstsize.c

--- libft/ft_lstsize.c
+++ libft/ft_lstsize.c
@@ -6,10 +6,42 @@
 /*   By: abeh <abeh@student.42singapore.sg>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/21 03:12:24 by abeh              #+#    #+#             */
-/*   Updated: 2024/05/21 14:35:24 by abeh             ###   ########.fr       */
+/*   Updated: 2024/05/21 21:29:45 by abeh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int ft_lstsize(t_list *lst);
+int ft_lstsize(t_list *lst)
+{
+    size_t i;
+    
+    i = 0;
+    while (lst)
+    {
+        i++;
+        lst = lst->next;
+    }
+    return(i);
+}
+int main(void)
+{
+    // Create nodes for the linked list
+    t_list *head = malloc(sizeof(t_list));
+    head->content = "Hello";
+    head->next = malloc(sizeof(t_list));
+    head->next->content = "World";
+    head->next->next = malloc(sizeof(t_list));
+    head->next->next->content = "Goodbye";
+    head->next->next->next = malloc(sizeof(t_list));
+    head->next->next->next->content = "World";
+    head->next->next->next->next = NULL;
+
+    // Call ft_lstsize to get the size of the linked list
+    int size = ft_lstsize(head);
+
+    // Print the result
+    printf("There are %d nodes in the list.\n", size);
+
+    return 0;
+}
