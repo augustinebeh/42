ec8d5e2328a1015211673afc350dc6a4e2501b60
Augustine Beh <enitsugua.b@gmail.com>
2024-05-21 17:04:04 +0800

    Update libft.h

--- libft/libft.h
+++ libft/libft.h
@@ -6,12 +6,13 @@
 /*   By: abeh <abeh@student.42singapore.sg>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/21 13:45:18 by abeh              #+#    #+#             */
-/*   Updated: 2024/05/21 17:03:03 by abeh             ###   ########.fr       */
+/*   Updated: 2024/05/21 17:03:58 by abeh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # ifndef LIBFT_H
 # define LIBFT_H
+
 # include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
