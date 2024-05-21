d27a7aea62c6bbf5d9052dec43903f9edd47b2fe
Augustine Beh <enitsugua.b@gmail.com>
2024-05-21 21:17:09 +0800

    Update ft_lstadd_front.c

--- libft/ft_lstadd_front.c
+++ libft/ft_lstadd_front.c
@@ -6,10 +6,53 @@
 /*   By: abeh <abeh@student.42singapore.sg>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/21 03:11:52 by abeh              #+#    #+#             */
-/*   Updated: 2024/05/21 14:35:02 by abeh             ###   ########.fr       */
+/*   Updated: 2024/05/21 21:16:25 by abeh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void ft_lstadd_front(t_list **lst, t_list *new);
+void ft_lstadd_front(t_list **lst, t_list *new)
+{
+    if (!lst)
+        return;
+    new->next = *lst;
+    *lst = new;
+}
+
+int main()
+{
+    // Initialize two nodes with some data
+    t_list *node1;
+    t_list *node2 = malloc(sizeof(t_list));
+    t_list *list; 
+    
+    node1 = malloc(sizeof(t_list));
+    node2 = malloc(sizeof(t_list));
+    node1->content = "Hello";
+    node2->content = "World";
+
+    // Add node2 to node1
+    ft_lstadd_front(&node1, node2);
+
+    // Print the data of each node in the list
+    t_list *current = node1;
+    int i = 1;
+    while (current!= NULL)
+    {
+        printf("Content in node %d is:  %s\n", i, (char *)current->content);
+        current = current->next;
+        i++;
+    }
+
+    // Free the memory allocated for the nodes
+    current = node1;
+    while (current!= NULL)
+    {
+        t_list *next = current->next;
+        free(current);
+        current = next;
+    }
+
+    return 0;
+}
