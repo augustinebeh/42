94df9d5c05fe6f76176f537bb9c5db812929f5ca
Augustine Beh <enitsugua.b@gmail.com>
2024-05-22 23:35:41 +0800

    Update ft_lstiter.c

--- libft/ft_lstiter.c
+++ libft/ft_lstiter.c
@@ -6,10 +6,13 @@
 /*   By: abeh <abeh@student.42singapore.sg>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/21 03:13:37 by abeh              #+#    #+#             */
-/*   Updated: 2024/05/21 14:35:13 by abeh             ###   ########.fr       */
+/*   Updated: 2024/05/22 23:23:27 by abeh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void ft_lstiter(t_list *lst, void (*f)(void *));
+void ft_lstiter(t_list *lst, void (*f)(void *))
+{
+    
+}
