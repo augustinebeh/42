981131d2cc8a52a4be88e32c40e42638a63e5a07
Augustine Beh <enitsugua.b@gmail.com>
2024-05-22 16:00:18 +0800

    Update ft_putstr_fd.c

--- libft/ft_putstr_fd.c
+++ libft/ft_putstr_fd.c
@@ -6,8 +6,24 @@
 /*   By: abeh <abeh@student.42singapore.sg>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 04:24:58 by abeh              #+#    #+#             */
-/*   Updated: 2024/05/16 04:25:00 by abeh             ###   ########.fr       */
+/*   Updated: 2024/05/22 15:59:22 by abeh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void ft_putstr_fd(char *s, int fd);
+#include "libft.h"
+
+void	ft_putstr_fd(char *s, int fd)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+	{
+		ft_putchar_fd(s[i], fd);
+		i++;
+	}
+}
+/* int	main(void)
+{
+	ft_putstr_fd("Hello, World!\n", 2);
+} */
