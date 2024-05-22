b83a6795b25e9e7cf4d5016892c69173be25980a
Augustine Beh <enitsugua.b@gmail.com>
2024-05-22 17:11:35 +0800

    Update ft_strmapi.c

--- libft/ft_strmapi.c
+++ libft/ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: abeh <abeh@student.42singapore.sg>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 04:24:23 by abeh              #+#    #+#             */
-/*   Updated: 2024/05/22 00:17:03 by abeh             ###   ########.fr       */
+/*   Updated: 2024/05/22 17:11:06 by abeh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,15 @@
 #include <stdlib.h>
 #include <string.h>
 
-char f_impl(unsigned int a, char b)
-{
-    return (b + a);
-}
+char	f_function(unsigned int a, char b);
 
-char		*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
 	unsigned int	i;
 	char			*str;
 
 	if (!s || !f)
-        return (NULL);
+		return (NULL);
 	i = 0;
 	str = malloc(sizeof(char) * (strlen(s)) + 1);
 	if (str == NULL)
@@ -39,22 +36,26 @@ char		*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 	return (str);
 }
 
-
-
-int main(void)
+/* char	f_function(unsigned int a, char b)
 {
-    char const *str = "aaaaaaaaaaa";
-    char (*f)(unsigned int, char) = &f_impl;
-    char *result = ft_strmapi(str, f);
-
-    if (result == NULL)
-    {
-        printf("Failed to allocate memory\n");
-        return (1);
-    }
-
-    printf("%s\n", result);
-    free(result);
-
-    return (0);
+	return (b - a);
 }
+
+int	main(void)
+{
+	char const	*str;
+	char		(*f)(unsigned int, char);
+	char		*result;
+
+	str = "I!NRZJ&`W^";
+	f = &f_function;
+	result = ft_strmapi(str, f);
+	if (result == NULL)
+	{
+		printf("Failed to allocate memory\n");
+		return (1);
+	}
+	printf("%s\n", result);
+	free(result);
+	return (0);
+} */
