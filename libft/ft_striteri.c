8393867c0f9140bc7ec0a2949c2f98055be2e57f
Augustine Beh <enitsugua.b@gmail.com>
2024-05-22 19:18:19 +0800

    Update ft_striteri.c

--- libft/ft_striteri.c
+++ libft/ft_striteri.c
@@ -6,8 +6,39 @@
 /*   By: abeh <abeh@student.42singapore.sg>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 04:24:34 by abeh              #+#    #+#             */
-/*   Updated: 2024/05/16 04:24:38 by abeh             ###   ########.fr       */
+/*   Updated: 2024/05/22 18:52:27 by abeh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void ft_striteri(char *s, void (*f)(unsigned int, char*));
+#include "libft.h"
+
+// Function that applies a function to each character in a string
+void	ft_striteri(char *s, void (*f)(unsigned int, char *))
+{
+	unsigned int	i;
+
+	if (!s)
+		return ;
+	i = 0;
+	while (s[i])
+	{
+		f(i, s + i);
+		i++;
+	}
+}
+
+/* void	f_function(unsigned int index, char *c)
+{
+	*c = *c - index;
+}
+
+int	main(void)
+{
+	char str[] = "I!NRZJ&`W^";
+	void (*f)(unsigned int, char *);
+    
+	f = f_function;
+	ft_striteri(str, f);
+	printf("%s\n", str);
+	return (0);
+} */
