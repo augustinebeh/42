0bfa40afceecada6ad8fcf37877212a03af36140
Augustine Beh <enitsugua.b@gmail.com>
2024-05-22 16:10:29 +0800

    Update ft_putendl_fd.c

--- libft/ft_putendl_fd.c
+++ libft/ft_putendl_fd.c
@@ -6,8 +6,26 @@
 /*   By: abeh <abeh@student.42singapore.sg>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 04:25:08 by abeh              #+#    #+#             */
-/*   Updated: 2024/05/16 04:25:10 by abeh             ###   ########.fr       */
+/*   Updated: 2024/05/22 16:08:57 by abeh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void ft_putendl_fd(char *s, int fd);
+#include "libft.h"
+
+void	ft_putendl_fd(char *s, int fd)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+	{
+		write(fd, &s[i], 1);
+		i++;
+	}
+	write(fd, "\n", 1);
+}
+/* int main(void)
+{
+    ft_putendl_fd("Hello World!", 1);
+    return(0);
+} */
