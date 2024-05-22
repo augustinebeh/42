8221cd89a2cb6fce1b6e21f53822a67cf448cc82
Augustine Beh <enitsugua.b@gmail.com>
2024-05-22 16:44:02 +0800

    Update ft_putnbr_fd.c

--- libft/ft_putnbr_fd.c
+++ libft/ft_putnbr_fd.c
@@ -6,8 +6,38 @@
 /*   By: abeh <abeh@student.42singapore.sg>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 04:25:17 by abeh              #+#    #+#             */
-/*   Updated: 2024/05/16 04:25:18 by abeh             ###   ########.fr       */
+/*   Updated: 2024/05/22 16:43:39 by abeh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void ft_putnbr_fd(int n, int fd);
+#include "libft.h"
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	if (n == -2147483648)
+		ft_putstr_fd("-2147483648", fd);
+	else
+	{
+		if (n < 0)
+		{
+			write(fd, "-", 1);
+			n = -n;
+		}
+		if (n > 9)
+		{
+			ft_putnbr_fd(n / 10, fd);
+			ft_putnbr_fd(n % 10, fd);
+		}
+		else
+		{
+			ft_putchar_fd((n + '0'), fd);
+		}
+	}
+}
+
+/* int	main(void)
+{
+	ft_putnbr_fd(189, 1);
+	ft_putchar_fd('\n', 1);
+	return (0);
+} */
