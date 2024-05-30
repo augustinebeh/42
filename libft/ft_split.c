db16cf75a4b1addd38ccdcff8adf3fd90a5c1b02
Augustine Beh <enitsugua.b@gmail.com>
2024-05-30 20:38:15 +0800

    OhMyGod, ft_split is driving me crazy

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,95 +6,101 @@
 /*   By: abeh <abeh@student.42singapore.sg>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 04:24:07 by abeh              #+#    #+#             */
-/*   Updated: 2024/05/30 20:16:55 by abeh             ###   ########.fr       */
+/*   Updated: 2024/05/30 20:37:43 by abeh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static void *ft_free(char **str, int count)
+static void	*ft_free(char **str, int count)
 {
-    int i;
-    
-    i = 0;
-    while (i < count)
-    {
-        free(str[i]);
-        i++;
-    }
-    free(str);
-    return (NULL);
-}
-static size_t count_ss(char const *s, char c)
-{
-    int len;
-    int count;
-    int i;
-    int previous_i;
-    
-    len = ft_strlen(s);
-    count = 0;
-    i = 0;
-    while (i < len)
-    {
-        while (i < len && s[i] == c)
-            i++;
-        previous_i = i;
-        while (i < len && s[i] != c)
-            i++;
-        if (i > previous_i)
-            count++;
-    }
-    return count;
+	int	i;
+
+	i = 0;
+	while (i < count)
+	{
+		free(str[i]);
+		i++;
+	}
+	free(str);
+	return (NULL);
 }
 
-static char *create_ss(const char *str, int start, int end)
+static size_t	count_ss(char const *s, char c)
 {
-    char *word;
-    int i;
-    
-    i = 0;
-    word = malloc((end - start + 1) * sizeof(char));
-    if (!word)
-        return (NULL);
-    while (start < end)
-    {
-        word[i] = str[start];
-        i++;
-        start++;
-    }
-    word[i] = 0;
-    return (word);
+	int	len;
+	int	count;
+	int	i;
+	int	previous_i;
+
+	len = ft_strlen(s);
+	count = 0;
+	i = 0;
+	while (i < len)
+	{
+		while (i < len && s[i] == c)
+			i++;
+		previous_i = i;
+		while (i < len && s[i] != c)
+			i++;
+		if (i > previous_i)
+			count++;
+	}
+	return (count);
 }
 
-char **ft_split(const char *s, char c)
+static char	*create_ss(const char *str, int start, int end)
 {
-    char **res;
-    size_t i;
-    int j;
-    int s_word;
-    
-    i = 0;
-    j = 0;
-    s_word = -1;
-    res = ft_calloc((count_ss(s, c) + 1), sizeof(char *));
-    if (!res)
-        return (NULL);
-    while (i <= ft_strlen(s))
-    {
-        if (s[i] != c && s_word < 0)
-            s_word = i;
-        else if ((s[i] == c || i == ft_strlen(s)) && s_word >= 0)
-        {
-            res[j] = create_ss(s, s_word, i);
-            if (!(res[j]))
-                return (ft_free(res, j));
-            s_word = -1;
-            j++;
-        }
-        i++;
-    }
-    return (res);
+	char	*word;
+	int		i;
+
+	i = 0;
+	word = malloc((end - start + 1) * sizeof(char));
+	if (!word)
+		return (NULL);
+	while (start < end)
+	{
+		word[i] = str[start];
+		i++;
+		start++;
+	}
+	word[i] = 0;
+	return (word);
+}
+
+static void init_variables(size_t *i, size_t  *j)
+{
+    *i = 0;
+    *j = 0;
+}
+
+char	**ft_split(const char *s, char c)
+{
+	char	**res;
+	size_t	i;
+	size_t		j;
+	int		s_word;
+
+	init_variables(&i, &j);
+	s_word = -1;
+	res = ft_calloc((count_ss(s, c) + 1), sizeof(char *));
+	if (!res)
+		return (NULL);
+	while (i <= ft_strlen(s))
+	{
+		if (s[i] != c && s_word < 0)
+			s_word = i;
+		else if ((s[i] == c || i == ft_strlen(s)) && s_word >= 0)
+		{
+			res[j] = create_ss(s, s_word, i);
+			if (!(res[j]))
+				return (ft_free(res, j));
+			s_word = -1;
+			j++;
+		}
+		i++;
+	}
+	return (res);
 }
 
 /* int	main(void)
