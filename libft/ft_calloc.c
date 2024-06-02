4e603fffa4f22af10df976e78242668bae51683d
Augustine <enitsugua.b@gmail.comls>
2024-06-02 19:19:31 +0800

    Update

--- libft/ft_calloc.c
+++ libft/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: abeh <abeh@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/14 22:34:50 by abeh              #+#    #+#             */
-/*   Updated: 2024/06/02 19:16:40 by abeh             ###   ########.fr       */
+/*   Updated: 2024/06/02 19:18:50 by abeh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,61 +17,61 @@ void	*ft_calloc(size_t nmemb, size_t size)
 	void	*res;
 
 	res = (void *)malloc(nmemb * size);
-    if (!res)
-        return (NULL);
-    ft_bzero(res, nmemb * size);
-    return (res);
+	if (!res)
+		return (NULL);
+	ft_bzero(res, nmemb * size);
+	return (res);
 }
 
 /* int main()
 {
-    int *array;
-    size_t nmemb = 3;
-    size_t size = sizeof(int);
-    size_t i = 0;
+	int *array;
+	size_t nmemb = 3;
+	size_t size = sizeof(int);
+	size_t i = 0;
 
-    array = (int *)malloc(nmemb*size); //allocates a memory to array
-    if (array == NULL)
-    {
-        printf("Memory alloc failed!\n");
-        return (1);
-    }
-    printf(" ------------------\n");
-    printf("|   AFTER MALLOC   |\n");
-    printf(" ------------------\n");
-    i = 0;
-     while (i < nmemb) //prints out the values of all the elements
-    {
-        printf("Array element %zu is %d\n", i, array[i]);
-        i++;
-    }
-    
-    printf(" ------------------\n");
-    printf("|  EDITTED MALLOC  |\n");
-    printf(" ------------------\n");
-    i = 0;
-    while (i < 8) // give all elment in the array the value of 1
-    {
-        array[i] = 1;
-        i++;
-    }
-    i = 0;
-    while (i < nmemb) //prints out the values of all the elements
-    {
-        printf("Array element %zu is %d\n", i, array[i]);
-        i++;
-    }
-    printf(" ------------------\n");
-    printf("|   AFTER CALLOC   |\n");
-    printf(" ------------------\n");
-    i = 0;
-    array = calloc(nmemb, size);
-    printf("After ft_calloc:\n");
-    while (i < nmemb)
-    {
-        printf("Array element %zu is %d\n", i, array[i]);
-        i++;
-    }
-    free(array);
-    return (0);
+	array = (int *)malloc(nmemb*size); //allocates a memory to array
+	if (array == NULL)
+	{
+		printf("Memory alloc failed!\n");
+		return (1);
+	}
+	printf(" ------------------\n");
+	printf("|   AFTER MALLOC   |\n");
+	printf(" ------------------\n");
+	i = 0;
+		while (i < nmemb) //prints out the values of all the elements
+	{
+		printf("Array element %zu is %d\n", i, array[i]);
+		i++;
+	}
+
+	printf(" ------------------\n");
+	printf("|  EDITTED MALLOC  |\n");
+	printf(" ------------------\n");
+	i = 0;
+	while (i < 8) // give all elment in the array the value of 1
+	{
+		array[i] = 1;
+		i++;
+	}
+	i = 0;
+	while (i < nmemb) //prints out the values of all the elements
+	{
+		printf("Array element %zu is %d\n", i, array[i]);
+		i++;
+	}
+	printf(" ------------------\n");
+	printf("|   AFTER CALLOC   |\n");
+	printf(" ------------------\n");
+	i = 0;
+	array = calloc(nmemb, size);
+	printf("After ft_calloc:\n");
+	while (i < nmemb)
+	{
+		printf("Array element %zu is %d\n", i, array[i]);
+		i++;
+	}
+	free(array);
+	return (0);
 } */
