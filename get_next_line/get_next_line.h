ecc9b681a79cea28f96f1562ac6e47d095b73fb2
Augustine Beh <enitsugua.b@gmail.com>
2024-06-09 18:36:07 +0800

    get_next_line Updates

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: abeh <abeh@student.42singapore.sg>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/31 22:21:03 by abeh              #+#    #+#             */
-/*   Updated: 2024/06/09 07:19:35 by abeh             ###   ########.fr       */
+/*   Updated: 2024/06/09 17:21:34 by abeh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,9 @@
 # include <stdlib.h>
 # include <unistd.h>
 
-# ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 10
-# endif
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 10
+#endif
 
 size_t	ft_strlen(const char *s);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
