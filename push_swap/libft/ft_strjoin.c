2dddd42612b26ef2a4f1801d524f42465104ea16
Augustine Beh <enitsugua.b@gmail.com>
2024-06-26 05:50:25 +0800

    Update

---
+++ push_swap/libft/ft_strjoin.c
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abeh <abeh@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/16 04:23:41 by abeh              #+#    #+#             */
+/*   Updated: 2024/05/31 16:30:05 by abeh             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char			*str;
+	unsigned int	i;
+	unsigned int	j;
+
+	i = 0;
+	j = 0;
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+	str = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (str == NULL)
+		return (NULL);
+	while (s1[i])
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+	{
+		str[i] = s2[j];
+		j++;
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
+/* int main(int argc, char **argv)
+{
+        char *str;
+
+    if (argc != 3)
+        printf("----------ERROR----------");
+    else
+    {
+        str = ft_strjoin(argv[1], argv[2]);
+        printf("-------------------------\n");
+        printf("The Input of the 1st String is: %s\n", argv[1]);
+        printf("The Input of the 2nd String is: %s\n", argv[2]);
+        printf("The output of ft_strjoin is   : %s\n", str);
+        
+        printf("-------------------------\n");
+        free(str);
+        return (0);    
+    }
+}
+
+ */
