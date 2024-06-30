e1d67fc0c730496891341fbcfbb4c195eab7f5a5
Augustine Beh <enitsugua.b@gmail.com>
2024-06-30 18:48:52 +0800

    Norminette Updates

--- push_swap/push_swap.h
+++ push_swap/push_swap.h
@@ -13,15 +13,17 @@
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
-typedef struct {
-	int	*data;
-	int	size;
-}		stack;
+typedef struct
+{
+	int		*data;
+	int		size;
+}			stack;
 
-typedef struct {
-	stack a;
-	stack b;
-}	stacks;
+typedef struct
+{
+	stack	a;
+	stack	b;
+}			stacks;
 
 # include <limits.h>
 # include <stddef.h>
@@ -30,30 +32,28 @@ typedef struct {
 # include <string.h>
 # include <unistd.h>
 
-int		highest_int(stack a);
-int		lowest_int(stack a);
-int		if_sorted(stack a);
-int		has_duplicates(stacks *s);
-int	is_valid_int(const char *str);
+int			highest_int(stack a);
+int			lowest_int(stack a);
+int			if_sorted(stack a);
+int			has_duplicates(stacks *s);
+int			is_valid_int(const char *str);
 
+void		swap_algo_three(stacks *s);
+void		swap_algo_four(stacks *s);
 
-void	swap_algo_three(stacks* s);
-void	swap_algo_four(stacks* s);
+void		pa(stacks *s);
+void		pb(stacks *s);
 
+void		sa(stacks *s, int flag);
+void		sb(stacks *s, int flag);
+void		ss(stacks *s, int flag);
 
-void	pa(stacks *s);
-void	pb(stacks *s);
+void		ra(stacks *s, int flag);
+void		rb(stacks *s, int flag);
+void		rr(stacks *s, int flag);
 
-void	sa(stacks *s, int flag);
-void	sb(stacks *s, int flag);
-void	ss(stacks *s, int flag);
-
-void	ra(stacks *s, int flag);
-void	rb(stacks *s, int flag);
-void	rr(stacks *s, int flag);
-
-void	rra(stacks *s, int flag);
-void	rrb(stacks *s, int flag);
-void	rrr(stacks *s, int flag);
+void		rra(stacks *s, int flag);
+void		rrb(stacks *s, int flag);
+void		rrr(stacks *s, int flag);
 
 #endif
